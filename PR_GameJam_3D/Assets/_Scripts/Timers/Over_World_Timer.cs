9d8672abe86c61b39aa299cf4c424d902aedc21a
Rui Santos <cardkingrui@gmail.com>
2022-08-23 01:29:28 -0400

    Timer
    Timer is done and made as a canvas prefab for ease of use. Currently working on the Level 1 Scene

---
+++ PR_GameJam_3D/Assets/_Scripts/Timers/Over_World_Timer.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Over_World_Timer : MonoBehaviour
+{
+    [SerializeField] int startTimer;
+    [SerializeField] int currentTimer;
+    [SerializeField] Text countdownText;
+    [SerializeField] Slider slider;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Being(startTimer);
+    }
+
+    private void Being(int Second)
+    {
+        currentTimer = Second;
+        slider.maxValue = Second;
+        slider.value = Second;
+        StartCoroutine(UpdateTimer());
+    }
+
+    private IEnumerator UpdateTimer()
+    {
+        while(currentTimer>= 0)
+        {
+            countdownText.text = $"{currentTimer / 60:00}:{currentTimer % 60:00}";
+            currentTimer--;
+            slider.value = currentTimer;
+            yield return new WaitForSeconds(1f);
+        }
+        OnEnd();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnEnd()
+    {
+        //On 0, end the game
+    }
+}
