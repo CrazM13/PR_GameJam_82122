2d6ea3c1cb3f69695e5bb8ce37c39b5727465213
Rui Santos <cardkingrui@gmail.com>
2022-08-24 18:17:29 -0400

    Level 1
    Added effect to drop off and win after getting 15 or more points.

--- PR_GameJam_3D/Assets/_Scripts/Anthill/Anthill_Collection.cs
+++ PR_GameJam_3D/Assets/_Scripts/Anthill/Anthill_Collection.cs
@@ -1,29 +1,57 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Anthill_Collection : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GroupController group = ServiceLocator.Instance.GroupController;
+    public GroupController group;
     [SerializeField] Over_World_Timer timer;
+    public float distance;
+    private int score;
+    [SerializeField] ParticleSystem dropOff;
     void Start()
     {
-        
+        group = ServiceLocator.Instance.GroupController;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timer.incrementTimer(group.GetCarryObjectScore() * 5);
+        group.ClearCarry();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Collider[] colliders = Physics.OverlapBox(this.transform.position,new Vector3(1,1,1));
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position,5);
         foreach(Collider collider in colliders)
         {
             GroupController groupController = collider.GetComponent<GroupController>();
             if (groupController)
             {
-                timer.incrementTimer((group.GetCarryObjectScore()*5));
+                timer.incrementTimer(group.GetCarryObjectScore()*5);
                 group.ClearCarry();
             }
         }
+
+        distance = Vector3.Distance(this.transform.position, group.transform.position);
+        if(distance <= 3 && (group.GetCarryObjectScore() > 0))
+        {
+            timer.incrementTimer(group.GetCarryObjectScore() * 5);
+            score += group.GetCarryObjectScore() * 5;
+            dropOff.Play();
+            group.ClearCarry();
+        }
+
+        if(score >= 15)
+        {
+            SceneManager.LoadScene(0);
+        }
+    }
+    public int getScore()
+    {
+        return score;
     }
 }
